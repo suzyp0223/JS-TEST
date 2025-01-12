018ff0835a363f8e69d24b6749b3286c2ca36899
suzy <suzyp0223@gmail.com>
2025-01-12 16:43:39 +0900

    인자2개 사이의수 음수전까지 무한뺄셈

--- 0algorithm/47infinitySubtraction.js
+++ 0algorithm/47infinitySubtraction.js
@@ -10,40 +10,58 @@ function answer(s, e) {
   sequence.push(s);
   sequence.push(e);
 
-  let x = 0;
-  let y = 0;
-  let z = 0;
-  // 포문으로 x-y=z 를 z가 음수가 될때까지 반복.
-  // for (let i = 0; i < sequence.length; i++) {
-  // console.log('[i]: ', sequence[i]);
+  let sum;
+  while (1) { //1=true true일때까지 계속 반복.
+    sum = s - e;
+    s = e;
+    e = sum;
 
-  x = sequence[0];
-  y = sequence[1];
-
-  if (sequence[0] - sequence[1] > -1) {
-    z = x - y;
-    sequence.push(x, y, z);
-    // console.log("z:", z);
-    console.log('sequence: ', sequence);
-  } else if (z - y < -1) {
-    z = z - y;
-    sequence.push(x, y, z);
-    // console.log("z:>>>", z);
-    console.log('sequence:>>> ', sequence);
+    if (e < 0) break;
+    sequence.push(e);
   }
-
-  for (const num of sequence) {
-    z = num[0] - num[1] > -1 ? sequence.push(z) : z = num[1] - z < -1;
-  }
-
-
-  // 배열에 첫x,y,z 와 음수 전의 z값 배열에 저장해서 반환
-
-
+  /*
+  [13, 7, 6, 1, 5]
+  13  7   6    1    5   -4
+  s   e  sum
+      s   e   sum
+          s    e   sum
+               s    e   sum
+                    s    e
+  */
 
   return sequence;
 }
-// 9-3=6 3-6=-3
+
+
+function answer(s, e) {
+  let sequence = [];
+  sequence.push(s);
+  sequence.push(e);
+
+  while (true) {
+    let diff = sequence[sequence.length - 2] - sequence[sequence.length - 1]; // 마지막 두 값의 차이
+    if (diff < 0) break; // 결과가 음수면 반복 종료
+    sequence.push(diff); // 결과를 배열에 추가
+  }
+
+  return sequence;
+}
+
+function answer(s, e) {
+  let sequence = []; // 결과를 저장할 배열
+  sequence.push(s); // 초기 숫자 s를 배열에 추가
+  sequence.push(e); // 초기 숫자 e를 배열에 추가
+
+  for (let i = 2; ; i++) { // i는 sequence 배열의 다음 인덱스
+    let diff = sequence[i - 2] - sequence[i - 1]; // 마지막 두 값의 차이
+    if (diff < 0) break; // 결과가 음수면 반복 종료
+    sequence.push(diff); // 결과를 배열에 추가
+  }
+
+  return sequence;
+}
+
+
 /* main code */
 let input = [
   // TC: 1
@@ -61,6 +79,6 @@ for (let i = 0; i < input.length; i++) {
   console.log(answer(input[i][0], input[i][1]));
 }
 // 출력 값
-// #1
-// #2
-// #3
+// #1 [9, 3, 6]
+// #2 [6, 3, 3, 0, 3]
+// #3 [13, 7, 6, 1, 5]
